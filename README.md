de02ea7ecbc35fae82a1f25bd41c820698a1c97c
juanIUPB <juanestebancastrillonurrea@pascualbravo.edu.co>
2023-02-10 12:35:07 -0500

    Actualizacion de ventas.py

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Algortimos
 Ejercicios de algoritmos resueltos en PYTHON y C#.
+
+- En el primera version del archivo encontraras el archivo resulto con programacion secuencial y en la siguiente actilizacion podras encontrar el ejercicio resulto con funciones.
--- ventas.py
+++ ventas.py
@@ -2,48 +2,40 @@
 NOTA: EL gerente desea conocer la venta del bien y el descuento se hace antes del IVA y el IVA del 16%.
 """
 
-#OPERACIONES
-
-    #Saca descuento del 5% al precio total 
-def descuentoDel_5(pagar):
-    pagar = (pagar*0.95*1.16)
-    return pagar
-
-    #saca descuento del 7%
-def descuentoDel_7(pagar):
-    pagar = (pagar*0.93*1.16)
-    return pagar  
-
-    #saca descuento del 18%
-def descuentoDel_18(pagar):
-    pagar = (pagar*0.82*1.16)
-    return pagar  
-
-    #saca descuento del 25%
-def descuentoDel_25(pagar):
-    pagar = (pagar*0.75*1.16)
-    return pagar  
-
-
-#CODIGO
-total=0
-m=int(input("Digite por favor la cantidad de usuarios: "))
-for i in range(m):
-  n=int(input("Digite por favor la cantidad de articulos: "))
+#FUNCIONES
+def usuarios(m):
+  total=0
   pagar=0
+  for i in range(m):
+    n=int(input("Digite por favor la cantidad de articulos: "))
+    pagar=Ciclointerno(n)
+    print("El valor a pagar es $",pagar)
+    total += pagar
+  print("El valor de las ventas fue de $",total)
+
+  return pagar
+
+def Ciclointerno(n):
   j=0
+  pagar=0
   while(j<n):
     valor=float(input("Digite por favor el valor del artículo: "))
-    pagar +=valor
+    pagar += valor
     j+=1
+  pagar=Condicion(pagar)
+  return pagar
+
+def Condicion(pagar):
   if(pagar < 100000):
-    pagar=descuentoDel_5(pagar)
+    pagar=(pagar*0.95*1.16)
   elif(pagar <  250001):
-    pagar=descuentoDel_7(pagar)
+    pagar=(pagar*0.93*1.16)
   elif(pagar < 600000):
-    pagar=descuentoDel_18(pagar)
+    pagar=(pagar*0.82*1.16)
   else:
-    pagar=descuentoDel_25(pagar)
-  print("El valor a pagar es $",pagar)
-  total+=pagar
-print("El valor de las ventas fue de $",total)
+    pagar=(pagar*0.75*1.16)
+  return pagar
+
+#CODIGO
+m=int(input("Digite por favor la cantidad de usuarios: "))
+total=usuarios(m)
