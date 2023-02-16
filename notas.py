0ccc298da413a1ad346f12dd239a0fba9cb31f71
juanIUPB <juanestebancastrillonurrea@pascualbravo.edu.co>
2023-02-15 22:16:44 -0500

    notas.py with functions

--- notas.py
+++ notas.py
@@ -1,27 +1,36 @@
 """ Elaborar un programa el cual le diga a cada usuario cual sera su nota definitiva en Herramientas de programacion 2 sabiendo que para esto el docente saca 5 notas. Se desea que almecene la nota definitiva de cada estudiante en un vector llamado notas y el nombre de cada estudiante en la misma posicion de su nota """
 
-usuarios=int(input("ingrese el numero de usuarios: "))
-nombre=list(map(str, input("digite por favor el nombre de los estudiantes: ").split()))
-notas=[]
-for i in range(usuarios):
-  print("Notas de ",nombre[i])
+def Llenar(usuarios):
+  notas=[]
+  nombre=list(map(str, input("digite por favor el nombre de los estudiantes: ").split()))
+  for i in range(usuarios):
+    print("Notas de ",nombre[i])
+    nf=NotaFinal()
+    notas.append(nf)
+  Reverse(notas, nombre)
+
+def NotaFinal():
   nf=0
   j=0
   while(j<5):
     nota=float(input("Digite por favor la nota: "))
     nf+=nota
     j+=1
-  nf/=5
-  notas.append(nf)
-for i in range(len(notas)-1):
-  for j in range(len(notas)-1):
-    if (notas[j] > notas[j+1]):
-      aux = notas[j]
-      notas[j] = notas[j+1]
-      notas[j+1] = aux
-      auxnom = nombre[j]
-      nombre[j] = nombre[j+1]
-      nombre[j+1] = auxnom
+  return nf/5
 
-print(nombre)
-print(notas)
+def Reverse(notas, nombres):
+  for i in range(len(notas)-1):
+    for j in range(len(notas)-1):
+      if notas[j] < notas[j+1]:
+        aux=notas[j]
+        notas[j]=notas[j+1]
+        notas[j+1]=aux
+        auxnom=nombres[j]
+        nombres[j]=nombres[j+1]
+        nombres[j+1]=auxnom
+  print(nombres)
+  print(notas)
+  
+#Llamar las funciones
+usuarios=int(input("ingrese el numero de usuarios: "))
+Llenar(usuarios)
